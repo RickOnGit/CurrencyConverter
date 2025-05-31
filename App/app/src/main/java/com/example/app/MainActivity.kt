1c621faf6a193570b8536e52857fe63222ad0298
rick <riccardoguadagnini7@gmail.com>
2025-05-31 22:07:48 +0200

    Tweaks

--- App/app/src/main/java/com/example/app/MainActivity.kt
+++ App/app/src/main/java/com/example/app/MainActivity.kt
@@ -17,6 +17,8 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.json.JSONObject
@@ -24,9 +26,9 @@ import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     private val myCoroutineScope = CoroutineScope(Dispatchers.IO)
-    var baseCurrency = "EUR"
-    var convertedToCurrency = "USD"
-    var conversionRate = 0f
+    var baseCurrency = "USD"
+    var convertedToCurrency = "EUR"
+    private var conversionRate = 0f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,27 +45,34 @@ class MainActivity : AppCompatActivity() {
 
     private fun textChanged() {
         val editText: EditText = findViewById(R.id.et_firstConversion)
+        var debounceJob: Job? = null
+
         editText.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
-                if (!s.isNullOrEmpty()) {
-                    getApiResult()
-                } else {
-                    Toast.makeText(applicationContext, "Type a value", Toast.LENGTH_SHORT).show()
+                debounceJob?.cancel()
+                debounceJob = myCoroutineScope.launch {
+                    delay(200)
+                    if (!s.isNullOrEmpty()) {
+                        withContext(Dispatchers.Main) {
+                            getApiResult()
+                        }
+                    } else {
+                        withContext(Dispatchers.Main) {
+                            getApiResult()
+                            Toast.makeText(applicationContext, "Type a value", Toast.LENGTH_SHORT).show()
+                        }
+                    }
                 }
             }
 
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                Log.d("Main", "Before Text Changed")
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                Log.d("Main", "OnTextChanged")
-            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
         })
     }
 
     private fun getApiResult() {
-        val API = "http://10.176.134.58:5000/convert?from=$baseCurrency&to=$convertedToCurrency"
+        //val API = "http://10.176.134.58:5000/convert?from=$baseCurrency&to=$convertedToCurrency"
+        val API = "http://192.168.0.206:5000/convert?from=$baseCurrency&to=$convertedToCurrency"
         myCoroutineScope.launch {
             try {
                 val apiResult: String = URL(API).readText()
@@ -92,35 +101,36 @@ class MainActivity : AppCompatActivity() {
     private fun spinnerSetup() {
         val spinner: Spinner = findViewById(R.id.spinner_firstConversion)
         val spinner2: Spinner = findViewById(R.id.spinner_secondConversion)
-        ArrayAdapter.createFromResource(
-            this,
-            R.array.currencies,
-            android.R.layout.simple_spinner_item,
-        ).also { adapter ->
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            spinner.adapter = adapter
-        }
 
-        ArrayAdapter.createFromResource(
+        val adapter = ArrayAdapter.createFromResource( //string arrays defined into res/values/strings.xml
+            this,
+            R.array.currencies, //string array to be used
+            android.R.layout.simple_spinner_item // standard android layout
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item) //object with the list of currencies + how to display those
+        spinner.adapter = adapter //the spinner gonna use the adapter<x>
+
+
+        val adapter2 = ArrayAdapter.createFromResource(
             this,
             R.array.currencies2,
             android.R.layout.simple_spinner_item,
-        ).also { adapter ->
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            spinner2.adapter = adapter
-        }
+        )
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner2.adapter = adapter2
+
 
         spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(parent: AdapterView<*>?) {
             }
 
             override fun onItemSelected(
-                parent: AdapterView<*>?,
+                parent: AdapterView<*>?, // equal to spinner<x>
                 view: View?,
                 position: Int,
                 id: Long,
             ) {
-                baseCurrency = parent?.getItemAtPosition(position).toString()
+                baseCurrency = parent?.getItemAtPosition(position).toString() //passing the x currency as base currency
                 getApiResult()
             }
         }
